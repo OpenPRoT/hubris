c3e619492b65ba340bfb3f8388171199c2603a7e
Will Marone <Will.Marone@amd.com>
2025-09-30 19:22:16 -0700

    crypto service initial commit

---
+++ drv/openprot-crypto-api/src/lib.rs
@@ -0,0 +1,32 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! API crate for Crypto server.
+
+#![no_std]
+
+use derive_idol_err::IdolError;
+use userlib::{sys_send, FromPrimitive};
+
+pub const ECDSA_SIG_SZ: usize = 96;
+
+/// Errors that can be produced from the hash server API.
+///
+/// This enumeration doesn't include errors that result from configuration
+/// issues, like sending host flash messages to some other task.
+#[derive(
+    Copy, Clone, Debug, FromPrimitive, Eq, PartialEq, IdolError, counters::Count,
+)]
+pub enum CryptoError {
+    NotInitialized = 1,
+    InvalidState,
+    Busy, // Some other owner is using the Hash block
+    NoData,
+
+    #[idol(server_death)]
+    ServerRestarted,
+}
+
+include!(concat!(env!("OUT_DIR"), "/client_stub.rs"));
+
