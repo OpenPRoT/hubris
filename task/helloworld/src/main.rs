607f524a38b4e9a5cabb77bd862f1c221d5abe29
Marvin Gudel <marvin.gudel@9elements.com>
2025-10-22 10:26:48 +0200

    Add RX data available notifications to UART driver
    Signed-off-by: Marvin Gudel <marvin.gudel@9elements.com>

--- task/helloworld/src/main.rs
+++ task/helloworld/src/main.rs
@@ -5,10 +5,9 @@
 #![no_std]
 #![no_main]
 
-use ast1060_uart_api::{Uart, UartError};
+use ast1060_uart_api::Uart;
 use embedded_io::{Read, Write};
 use userlib::*;
-use zerocopy::FromBytes;
 
 task_slot!(UART, uart_driver);
 
@@ -16,40 +15,23 @@ task_slot!(UART, uart_driver);
 fn main() -> ! {
     let mut uart = Uart::new(UART.get_task_id()).unwrap_lite();
     uart.write_fmt(format_args!("Hello World!\n")).unwrap_lite();
+
+    // Enable RX notifications.
+    // Print errors and continue (blocking the task with recv).
+    let _ = uart
+        .enable_rx_notification(notifications::RX_DATA_BIT)
+        .inspect_err(|e| {
+            uart.write_fmt(format_args!(
+                "enable_rx_notification failed: {e:?}\n"
+            ))
+            .unwrap_lite();
+        });
     let mut buf = [0; 32];
     loop {
-        hl::sleep_for(1);
+        sys_recv_notification(notifications::RX_DATA_MASK);
         let n = uart.read(&mut buf).unwrap_lite();
         uart.write_all(&buf[..n]).unwrap_lite();
     }
 }
 
-fn uart_send(text: &[u8]) {
-    let peer = UART.get_task_id();
-
-    const OP_WRITE: u16 = 1;
-    let (code, _) =
-        sys_send(peer, OP_WRITE, &[], &mut [], &[Lease::from(text)]);
-    assert_eq!(0, code);
-}
-
-fn uart_read<'a>(text: &'a mut [u8]) -> &'a [u8] {
-    let peer = UART.get_task_id();
-    const OP_READ: u16 = 2;
-
-    let mut response = [0u8; 4];
-    let (code, n) = sys_send(
-        peer,
-        OP_READ,
-        &[],
-        &mut response,
-        &mut [Lease::from(&mut *text)],
-    );
-    // check for success or overflow
-    if (code == 0 || code == 5) && n == 4 {
-        let n = u32::ref_from_bytes(&response[..n]).unwrap_lite();
-        &text[..*n as usize]
-    } else {
-        &[]
-    }
-}
+include!(concat!(env!("OUT_DIR"), "/notifications.rs"));
