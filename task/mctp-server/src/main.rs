2765efb4578c424d2440429b80e3a515ab616d4a
Marvin Gudel <marvin.gudel@9elements.com>
2025-11-03 15:58:08 +0100

    Implement a MCTP driver with serial transport binding
    - MCTP IPC definition & API for server and client
    - MCTP server with AST1060 UART transport binding
    - App with MCTP echo task
    - Tools for integration testing
      - Linux socket API based test application
      - Script for setting up QEMU and Linux-MCTP
    
    Signed-off-by: Marvin Gudel <marvin.gudel@9elements.com>

---
+++ task/mctp-server/src/main.rs
@@ -0,0 +1,155 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#![no_std]
+#![no_main]
+
+use ast1060_pac as device;
+use core::cell::RefCell;
+use core::ops::Deref;
+use userlib::*;
+
+use lib_ast1060_uart::Usart;
+
+mod serial;
+mod server;
+use server::Server;
+
+/// Maximum number of concurrent requests the server can handle.
+pub const MAX_REQUESTS: usize = 8;
+/// Maximum number of listeners that can be registered concurrently.
+pub const MAX_LISTENERS: usize = 8;
+/// Maximum number of concurrent outstanding receive calls.
+pub const MAX_OUTSTANDING: usize = 16;
+
+#[export_name = "main"]
+fn main() -> ! {
+    let mut msg_buf = [0; ipc::INCOMING_SIZE];
+    let peripherals = unsafe { device::Peripherals::steal() };
+
+    let usart = RefCell::new(Usart::from(peripherals.uart.deref()));
+    let serial_sender = serial::SerialSender::new(&usart);
+    let mut serial_reader = mctp_stack::serial::MctpSerialHandler::new();
+
+    let mut server: Server<_, MAX_OUTSTANDING> =
+        Server::new(mctp::Eid(42), 0, serial_sender);
+    let state = sys_get_timer();
+    server.update(state.now);
+
+    loop {
+        let msg = sys_recv_open(
+            &mut msg_buf,
+            notifications::UART_IRQ_MASK | notifications::TIMER_MASK,
+        );
+
+        if msg.sender == TaskId::KERNEL
+            && (msg.operation & notifications::UART_IRQ_MASK) != 0
+        {
+            let interrupt = usart.borrow_mut().read_interrupt_status();
+            if let Some(pkt) = serial::handle_uart_interrupt(
+                interrupt,
+                &usart,
+                &mut serial_reader,
+            ) {
+                server.stack.inbound(pkt.unwrap_lite()).unwrap_lite();
+                let state = sys_get_timer();
+                server.update(state.now);
+            }
+            sys_irq_control(notifications::UART_IRQ_MASK, true);
+            continue;
+        }
+
+        if msg.sender == TaskId::KERNEL
+            && (msg.operation & notifications::TIMER_MASK) != 0
+        {
+            let state = sys_get_timer();
+            server.update(state.now);
+            continue;
+        }
+
+        handle_mctp_msg(&msg_buf, msg, &mut server);
+    }
+}
+
+mod ipc {
+    use counters::*;
+    pub use mctp_api::ipc::*;
+
+    include!(concat!(env!("OUT_DIR"), "/server_stub.rs"));
+}
+
+fn handle_mctp_msg<S: mctp_stack::Sender, const OUTSTANDING: usize>(
+    msg_buf: &[u8],
+    recv_msg: RecvMessage,
+    server: &mut server::Server<S, OUTSTANDING>,
+) {
+    use hubpack::deserialize;
+    use idol_runtime::Leased;
+    use zerocopy::FromBytes;
+    let Some(op) = ipc::MCTPOperation::from_u32(recv_msg.operation) else {
+        // TODO check which cases and unwraps have to be handled better.
+        return;
+    };
+
+    let msg_buf = &msg_buf[..recv_msg.message_len];
+    match op {
+        ipc::MCTPOperation::req => {
+            let eid = ipc::MCTP_req_ARGS::ref_from_bytes(msg_buf)
+                .unwrap_lite()
+                .eid;
+            server.req(&recv_msg, eid);
+        }
+        ipc::MCTPOperation::listener => {
+            let typ = ipc::MCTP_listener_ARGS::ref_from_bytes(msg_buf)
+                .unwrap_lite()
+                .typ;
+            server.listener(&recv_msg, typ);
+        }
+        ipc::MCTPOperation::get_eid => {
+            server.get_eid(&recv_msg);
+        }
+        ipc::MCTPOperation::set_eid => {
+            let eid = ipc::MCTP_set_eid_ARGS::ref_from_bytes(msg_buf)
+                .unwrap_lite()
+                .eid;
+            server.set_eid(&recv_msg, eid);
+        }
+        ipc::MCTPOperation::recv => {
+            let (recv_args, _): (ipc::MCTP_recv_ARGS, _) =
+                deserialize(msg_buf).unwrap_lite();
+            let lease = Leased::write_only_slice(recv_msg.sender, 0, None)
+                .unwrap_lite();
+            server.recv(
+                recv_msg,
+                recv_args.handle,
+                recv_args.timeout_millis,
+                lease,
+            );
+        }
+        ipc::MCTPOperation::send => {
+            let (send_args, _): (ipc::MCTP_send_ARGS, _) =
+                deserialize(msg_buf).unwrap_lite();
+            let ic = send_args.raw_ic != 0;
+            let lease =
+                Leased::read_only_slice(recv_msg.sender, 0, None).unwrap_lite();
+            server.send(
+                &recv_msg,
+                send_args.handle,
+                send_args.typ,
+                send_args.eid,
+                send_args.tag,
+                ic,
+                lease,
+            );
+        }
+        ipc::MCTPOperation::drop => {
+            let handle = ipc::MCTP_drop_ARGS::ref_from_bytes(msg_buf)
+                .unwrap_lite()
+                .handle;
+            server.unbind(&recv_msg, handle);
+        }
+    }
+}
+
+include!(concat!(env!("OUT_DIR"), "/notifications.rs"));
---
+++ task/mctp-server/src/server.rs
@@ -0,0 +1,261 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+use crate::ipc::*;
+use heapless::LinearMap;
+use idol_runtime::{Leased, R, W};
+use mctp::{Eid, MsgIC, MsgType, Tag, TagValue};
+use mctp_stack::{AppCookie, Router};
+use userlib::*;
+use zerocopy::IntoBytes;
+
+// TODO: Use configuration from mctp-lib (mctp-estack)
+//       see https://github.com/OpenPRoT/mctp-lib/issues/4
+const MAX_PAYLOAD: usize = 1023;
+
+pub struct Server<S: mctp_stack::Sender, const OUTSTANDING: usize> {
+    pub stack: Router<S, { super::MAX_LISTENERS }, { super::MAX_REQUESTS }>,
+    /// The currently outstanding recv calls
+    ///
+    /// Maps the handle to RecvMessage that must be replied to,
+    /// once a message arrives or a timeout occurs.
+    /// The timeout is stored as a deadline.
+    outstanding: LinearMap<GenericHandle, (RecvMessage, u64), OUTSTANDING>,
+}
+
+impl<S: mctp_stack::Sender, const OUTSTANDING: usize> Server<S, OUTSTANDING> {
+    /// Create a new MCTP server instance
+    pub fn new(own_eid: mctp::Eid, now_millis: u64, outbound: S) -> Self {
+        let stack = Router::new(own_eid, now_millis, outbound);
+        Self {
+            stack,
+            outstanding: LinearMap::new(),
+        }
+    }
+
+    /// Answer a request for a handle for sending messages to the given EID
+    pub fn req(&mut self, msg: &RecvMessage, eid: u8) {
+        match self.stack.req(mctp::Eid(eid)) {
+            Ok(handle) => {
+                let handle = GenericHandle(handle.0 as u32); // TODO better mapping
+                sys_reply(msg.sender, 0, handle.as_bytes())
+            }
+            Err(e) => {
+                sys_reply(msg.sender, ServerError::from(e).into(), &[]);
+            }
+        }
+    }
+
+    /// Answer a request for a listener handle for incoming messages of the given type
+    pub fn listener(&mut self, msg: &RecvMessage, typ: u8) {
+        match self.stack.listener(mctp::MsgType(typ)) {
+            Ok(handle) => {
+                let handle = GenericHandle(handle.0 as u32); // TODO better mapping
+                sys_reply(msg.sender, 0, handle.as_bytes())
+            }
+            Err(e) => {
+                sys_reply(msg.sender, ServerError::from(e).into(), &[]);
+            }
+        }
+    }
+
+    /// Reply to a request for the current EID
+    pub fn get_eid(&mut self, msg: &RecvMessage) {
+        sys_reply(msg.sender, 0, self.stack.get_eid().0.as_bytes());
+    }
+
+    /// Set the current EID of the stack
+    pub fn set_eid(&mut self, msg: &RecvMessage, eid: u8) {
+        match self.stack.set_eid(mctp::Eid(eid)) {
+            Ok(()) => sys_reply(msg.sender, 0, &[]),
+            Err(e) => {
+                sys_reply(msg.sender, ServerError::from(e).into(), &[]);
+            }
+        }
+    }
+
+    /// Check for incoming messages for the handle given by the client
+    ///
+    /// Postpones the reply if no message is available.
+    /// If called again for a handle that already has a pending recv call,
+    /// try to answer and remove the handle.
+    /// Won't overwrite an existing pending recv call for the given handle.
+    pub fn recv(
+        &mut self,
+        msg: RecvMessage,
+        handle: GenericHandle,
+        timeout_millis: u32,
+        buf: Leased<W, [u8]>,
+    ) {
+        // Check for a message for the given handle, if there is one, copy it into buf and reply.
+        // Otherwise postpone the reply until a message arrives or a timeout occurs.
+        // For this the RecvMessage must be stored.
+        // On inbound message arrival, all postponed recv calls must be checked for a match.
+
+        let cookie = AppCookie(handle.0 as usize);
+        if let Some(mctp_message) = self.stack.recv(cookie) {
+            send_reply(&msg, mctp_message, buf);
+
+            // Remove any outstanding recv call for this handle, if any
+            self.outstanding.remove(&handle);
+            return;
+        }
+
+        let deadline = if timeout_millis != 0 {
+            self.set_timer(timeout_millis)
+        } else {
+            0
+        };
+
+        // We don't want to update any existing entries
+        if !self.outstanding.contains_key(&handle) {
+            self.outstanding
+                .insert(handle, (msg, deadline))
+                .unwrap_lite();
+        }
+    }
+
+    /// Send a message provided by the client
+    ///
+    /// Blocks until the message is sent or an error occurs.
+    /// When responding to a request received by a listener, `eid` and `tag` have to be set.
+    /// A request never sets an `eid`.
+    /// We distinguish request and response by the `handle` parameter: if it is `None`, it is a response.
+    /// When no `tag` is supplied for a request, a new one will be allocated.
+    pub fn send(
+        &mut self,
+        msg: &RecvMessage,
+        handle: Option<GenericHandle>,
+        typ: u8,
+        eid: Option<u8>,
+        tag: Option<u8>,
+        ic: bool,
+        buf: Leased<R, [u8]>,
+    ) {
+        // TODO figure out if handling incoming packets while sending is neccessary.
+        //      Having dedicated driver tasks with buffering for every transport would simplify this.
+
+        let mut msg_buf = [0; MAX_PAYLOAD];
+        if msg_buf.len() < buf.len() {
+            sys_reply(msg.sender, ServerError::NoSpace.into(), &[]);
+        }
+        if buf.read_range(0..buf.len(), &mut msg_buf).is_err() {
+            todo!("client died?");
+        }
+        let msg_buf = &msg_buf[..buf.len()];
+        let tag = if handle.is_none() {
+            // All calls without a handle are responses and must use unowned tags
+            // TODO erroring out if no tag is provided?
+            tag.map(|x| Tag::Unowned(TagValue(x)))
+        } else {
+            // A call with a handle is a request and must use owned tags (or allocate a new one)
+            tag.map(|x| Tag::Owned(TagValue(x)))
+        };
+        let res = self.stack.send(
+            eid.map(Eid),
+            MsgType(typ),
+            tag,
+            MsgIC(ic),
+            AppCookie(handle.unwrap_or(GenericHandle(255)).0 as usize), // Responses need no handle, use 255 as dummy
+            msg_buf,
+        );
+
+        match res {
+            Ok(tag) => {
+                let mut buf = [0; 8];
+                let l =
+                    hubpack::serialize(&mut buf, &tag.tag().0).unwrap_lite();
+                sys_reply(msg.sender, 0, &buf[..l]);
+            }
+            Err(e) => {
+                sys_reply(msg.sender, ServerError::from(e).into(), &[]);
+            }
+        }
+    }
+
+    /// Update the stack, check for receive calls that can be fullfilled
+    ///
+    /// Should be called whenever a timer interrupt occurs.
+    /// Sets the timer to the next timeout required.
+    pub fn update(&mut self, now_millis: u64) {
+        // Crash the server if updating the stack fails (this is probably unrecoverable)
+        let stack_timeout = self.stack.update(now_millis).unwrap_lite() as u32;
+        self.set_timer(stack_timeout);
+
+        let mut marked: heapless::Vec<_, OUTSTANDING> = heapless::Vec::new();
+
+        for (handle, (msg_handle, deadline)) in self.outstanding.iter() {
+            if let Some(mctp_msg) =
+                self.stack.recv(AppCookie(handle.0 as usize))
+            {
+                let lease =
+                    Leased::write_only_slice(msg_handle.sender, 0, None)
+                        .unwrap_lite();
+                send_reply(msg_handle, mctp_msg, lease);
+                let _ = marked.push(*handle);
+            }
+
+            if *deadline != 0 && now_millis >= *deadline {
+                sys_reply(msg_handle.sender, ServerError::TimedOut.into(), &[]);
+                let _ = marked.push(*handle);
+            }
+        }
+        for key in marked {
+            self.outstanding.remove(&key);
+        }
+    }
+
+    /// Set the system timer to expire after the given timeout returning a deadline
+    ///
+    /// Timer won't be set if the timeout is longer than the current one.
+    fn set_timer(&self, timeout_millis: u32) -> u64 {
+        let state = sys_get_timer();
+        // If a deadline is set and it is sooner than the requested timeout,
+        // keep it and return the deadline for the requested timeout by calulating it.
+        if let Some(deadline) = state.deadline {
+            if (deadline - state.now) < (timeout_millis as u64) {
+                return state.now + timeout_millis as u64;
+            }
+        }
+
+        // Else set the timer and return the deadline
+        set_timer_relative(timeout_millis, super::notifications::TIMER_MASK)
+    }
+
+    /// Unbind a handle previously allocated by `req` or `listener`
+    pub fn unbind(&mut self, msg: &RecvMessage, handle: GenericHandle) {
+        let cookie = AppCookie(handle.0 as usize);
+        let _ = self.stack.unbind(cookie);
+        self.outstanding.remove(&handle); // should never contain the handle at this point, but just in case...
+        sys_reply(msg.sender, 0, &[]);
+    }
+}
+
+fn send_reply(
+    msg: &RecvMessage,
+    mctp_message: mctp_stack::MctpMessage<'_>,
+    buf: Leased<W, [u8]>,
+) {
+    if mctp_message.payload.len() > buf.len() {
+        sys_reply(msg.sender, ServerError::NoSpace.into(), &[]);
+        return;
+    }
+    if buf
+        .write_range(0..mctp_message.payload.len(), mctp_message.payload)
+        .is_err()
+    {
+        todo!("client died?")
+    }
+    let answer = crate::ipc::RecvMetadata {
+        msg_typ: mctp_message.typ.0,
+        msg_ic: mctp_message.ic.0,
+        msg_tag: mctp_message.tag.tag().0,
+        remote_eid: mctp_message.source.0,
+        size: mctp_message.payload.len() as u64,
+    };
+
+    let mut msg_buf = [0; MAX_PAYLOAD];
+    let l = hubpack::serialize(&mut msg_buf, &answer).unwrap_lite();
+    sys_reply(msg.sender, 0, &msg_buf[..l]);
+}
